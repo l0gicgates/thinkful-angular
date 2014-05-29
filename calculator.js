d043ada296ff5adbe9a63e55af97e3e309243c12
Juan <jualin289@gmail.com>
2014-05-29 17:49:44 -0400

    Beta version

--- calculator.js
+++ calculator.js
@@ -1,6 +1,13 @@
 function plus() {
 	$("#result").empty();	// empty it if full
-	var result=parseInt($("#x").val()) + parseInt($("#y").val());
+	var x = parseInt($("#x").val());
+	var y = parseInt($("#y").val());
+
+	if(!isValid(x,y)){
+		return;
+	}
+
+	var result=x + y;
 	var textResult=$("#x").val() + ' plus ' + $("#y").val() + ' is ' + result.toString();
 	$("#result").append(textResult);
 	
@@ -8,8 +15,29 @@ function plus() {
 
 function minus() {
 	$("#result").empty();	// empty it if full
-	var result=parseInt($("#x").val()) - parseInt($("#y").val());
+	var x = parseInt($("#x").val());
+	var y = parseInt($("#y").val());
+
+	if(!isValid(x,y)){
+		return;
+	}
+
+
+	var result=x - y;
 	var textResult=$("#x").val() + ' minus ' + $("#y").val() + ' is ' + result.toString();
 	$("#result").append(textResult);
 	
+}
+
+function isValid(x,y) {
+
+	if(isNaN(x) || isNaN(y)) {
+		$("#result").append("Invalid input please try again");
+		return false;
+	}
+	else 
+		return true;
+
+
+
 }
